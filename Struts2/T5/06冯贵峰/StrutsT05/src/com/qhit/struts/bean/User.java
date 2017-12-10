49bf670488851934edc066a3dfce640ea5e2f034
Fengguifeng <2726174882@qq.com>
2017-12-10 23:24:47 +0800

    06 冯贵峰提交struts2 T05上机作业

---
+++ Struts2/T5/06冯贵峰/StrutsT05/src/com/qhit/struts/bean/User.java
@@ -0,0 +1,60 @@
+package com.qhit.struts.bean;
+
+public class User {
+	private int id;
+   private String uname;
+   private String upassword;
+   private String birthday;
+   private String sex;
+   private int enable;
+public User(int id, String uname, String upassword, String birthday,
+		String sex, int enable) {
+	super();
+	this.id = id;
+	this.uname = uname;
+	this.upassword = upassword;
+	this.birthday = birthday;
+	this.sex = sex;
+	this.enable = enable;
+}
+public User() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getUpassword() {
+	return upassword;
+}
+public void setUpassword(String upassword) {
+	this.upassword = upassword;
+}
+public String getBirthday() {
+	return birthday;
+}
+public void setBirthday(String birthday) {
+	this.birthday = birthday;
+}
+public String getSex() {
+	return sex;
+}
+public void setSex(String sex) {
+	this.sex = sex;
+}
+public int getEnable() {
+	return enable;
+}
+public void setEnable(int enable) {
+	this.enable = enable;
+}
+}
---
+++ Struts2/T5/06冯贵峰/StrutsT05/src/com/qhit/struts/dao/impl/UserDaoImpl.java
@@ -0,0 +1,101 @@
+package com.qhit.struts.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.qhit.struts.bean.User;
+import com.qhit.struts.dao.UserDao;
+import com.qhit.struts.utils.DBManager;
+
+public class UserDaoImpl implements UserDao {
+    private Connection con;
+    private PreparedStatement ps;
+    
+	@Override
+	public int addUser(User user) throws Exception {
+		con = DBManager.getConnection();
+		String sql = "insert into t_user values (?,?,?,?,?)";
+		ps = con.prepareStatement(sql);
+		ps.setString(1, user.getUname());
+		ps.setString(2, user.getUpassword());
+		ps.setString(3, user.getBirthday());
+		ps.setString(4, user.getSex());
+		ps.setInt(5, user.getEnable());
+		int row = ps.executeUpdate();
+		DBManager.close(ps, con);
+		return row;
+	}
+
+	@Override
+	public int deleteUser(User user) throws Exception {
+		con = DBManager.getConnection();
+		String sql = "delete from t_user where id = ?";
+		ps = con.prepareStatement(sql);
+		ps.setInt(1,user.getId());
+		int row = ps.executeUpdate();
+		DBManager.close(ps, con);
+		return row;
+	}
+
+	@Override
+	public int updateUser(User user) throws Exception {
+		con = DBManager.getConnection();
+		String sql = "update t_user set uname = ?, upwd = ?, birthday = ?, sex = ?, enable = ? where id = ?";
+		ps = con.prepareStatement(sql);
+		ps.setString(1, user.getUname());
+		ps.setString(2, user.getUpassword());
+		ps.setString(3, user.getBirthday());
+		ps.setString(4, user.getSex());
+		ps.setInt(5, user.getEnable());
+		ps.setInt(6, user.getId());
+		int row = ps.executeUpdate();
+		DBManager.close(ps, con);
+		return row;
+	}
+
+	@Override
+	public List<User> getAllUser() throws Exception {
+		User user = null;
+		List<User> users = new ArrayList<User>();
+		con = DBManager.getConnection();
+		String sql = "select * from t_user";
+		ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			user = new User(
+					rs.getInt("id"), 
+					rs.getString("uname"), 
+					rs.getString("upassword"), 
+					rs.getString("birthday"), 
+					rs.getString("sex"), 
+					rs.getInt("enable"));
+			users.add(user);
+		}
+		DBManager.close(ps, con);
+		return users;
+	}
+
+	@Override
+	public User getUserById(User user) throws Exception {
+		con = DBManager.getConnection();
+		String sql = "select * from t_user where id = ?";
+		ps = con.prepareStatement(sql);
+		ps.setInt(1, user.getId());
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			user = new User(
+					rs.getInt("id"), 
+					rs.getString("uname"), 
+					rs.getString("upassword"), 
+					rs.getString("birthday"), 
+					rs.getString("sex"), 
+					rs.getInt("enable"));
+		}
+		DBManager.close(ps, con);
+		return user;
+	}
+
+}
