ea25424a4b59038cb3a66c5f1446785d4955ffdd
Fengguifeng <2726174882@qq.com>
2017-12-30 11:15:09 +0800

    06冯贵峰提交hibernate t08上机作业

---
+++ Hibernate/T8/06冯贵峰/上机作业/HBT08/src/com/qhit/lh/gr3/fgf/hbt8/dao/BaseDao.java
@@ -0,0 +1,31 @@
+package com.qhit.lh.gr3.fgf.hbt8.dao;
+
+import java.util.List;
+
+public interface BaseDao {
+   /**
+ * @param obj
+ * 增
+ */
+public void add(Object obj);
+   /**
+ * @param obj
+ * 删
+ */
+public void delete(Object obj);
+   /**
+ * @param obj
+ * 改
+ */
+public void update(Object obj);
+/**
+ * 查
+ */
+public List<Object> gerAll(String fromObject);
+/**
+ * @param clazz
+ * @param id
+ * @return
+ */
+public  Object GetObjectById(Class clazz,int id);
+}
